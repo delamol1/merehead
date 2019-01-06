07c2ef43a6b75e572d9f01f30564fe8c0f31761d
Alexandr Hubenko <alexandrhubenko@Alexandrs-MacBook-Air.local>
2019-01-06 17:11:50 +0200

    Updates

---
+++ precache-manifest.1644b16c4f5b1f0ada52d91c5b3c73e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61f00bcae0081573fc0a",
+    "url": "/merehead/static/js/runtime~main.61f00bca.js"
+  },
+  {
+    "revision": "0facafc0efd1353e8510",
+    "url": "/merehead/static/js/main.0facafc0.chunk.js"
+  },
+  {
+    "revision": "1e5355589f683bb50a45",
+    "url": "/merehead/static/js/1.1e535558.chunk.js"
+  },
+  {
+    "revision": "0facafc0efd1353e8510",
+    "url": "/merehead/static/css/main.1fd1ba68.chunk.css"
+  },
+  {
+    "revision": "275c1b34489d506ffa4c42f00bdbc0c1",
+    "url": "/merehead/index.html"
+  }
+];
